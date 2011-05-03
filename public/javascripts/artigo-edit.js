14687249e31456193e626bdf6ece26a66e3200d0
Jared <codeimpossible@gmail.com>
2011-05-02 22:06:38 -0400

    added post preview

--- public/javascripts/artigo-edit.js
+++ public/javascripts/artigo-edit.js
@@ -40,8 +40,9 @@ function unpublish() {
 }
 
 function put(data, success, error) {
+    var postid = window.location.href.substring( window.location.href.indexOf('/posts/') + 7 ).replace(/\/edit/ig, '') * 1; 
 	$.ajax({
-		url: '/posts/<%= @post.id %>.json',
+		url: '/posts/' + postid + '.json',
 		data: data,
 		dataType: 'json',
 		type: 'PUT',
@@ -50,6 +51,27 @@ function put(data, success, error) {
 	});
 }
 
+function preview(e) {
+    e.preventDefault();
+    var context = $('.edit').size() > 0 ? '.edit' : '.new';
+    $('.preview #title').html( $('#title', context).val() );
+    $('.preview #summary').html( $('#summary', context).val() );
+    $('.preview #content').html( $('#content', context).val() );
+    $('.container').animate({
+        left: "-=800px"
+    }, 750);
+    return false;
+}
+
+function goback(e) {
+    e.preventDefault();
+    $('.container').animate({
+        left: "+=800px"
+    }, 500); 
+    return false;
+}
+
+
 (function($) {
 	$(function() {
 		$('button#publish').click(publish);
@@ -57,5 +79,7 @@ function put(data, success, error) {
 		$('#date_type').change(function(){
 			$('#post_created_at').toggle( $(this).val() === "custom" );
 		});
+        $('.edit button#preview, .new button#preview').click(preview);
+        $('.preview button#goback').click(goback);
 	});
 })(jQuery);
