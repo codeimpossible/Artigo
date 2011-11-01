2718ae6df781e6268b575078223fe7dc97bd0b8d
Jared Barboza <codeimpossible@gmail.com>
2011-11-01 00:23:18 -0400

    ummmm not sure what I was thinking with this file... all it does it break the tests :D

--- app/controllers/admin/appearance_controller.rb
+++
@@ -1,8 +0,0 @@
-class Admin::AppearanceController < ApplicationController
-  def index
-	rt = ::Rails.root.to_s
-	Dir.chdir("#{rt}/app/views/themes")
-	@themes = Dir['*/'].collect{ |t| t.chomp("/") }
-	@
-  end
-end
