371b188c61e743028ef0f7e798a6ffc016ff0251
Jared Barboza <jared.m.barboza@gmail.com>
2011-12-22 10:15:41 -0500

    Media Library initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111031155446) do
+ActiveRecord::Schema.define(:version => 20111219002228) do
+
+  create_table "medias", :force => true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "path"
+    t.string   "content_type"
+    t.integer  "size"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
