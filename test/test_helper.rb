2e1497838f9e0bdd627109399b1e9a3c962dc491
Jared Barboza <jared.m.barboza@gmail.com>
2011-12-29 15:00:30 -0500

    Test Cleanup/Speed Improvement
    Added a GC hack from 37signals blog to speed up unit tests, removed a
    puts statement that was causing a newline to show up in the unit test
    runner. Added Gemfile.lock to the gitignore.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,4 +11,42 @@ class ActiveSupport::TestCase
     login_as user
     yield if block_given?
   end
+
+  #
+  # Unit test memory optimizations
+  # Courtesy Jamis @ 37Signals
+  # http://37signals.com/svn/posts/2742-the-road-to-faster-tests
+  #
+  unless defined?(DEFERRED_GC_THRESHOLD)
+    setup :begin_gc_deferment
+    teardown :reconsider_gc_deferment
+
+    DEFERRED_GC_THRESHOLD = (ENV['DEFER_GC'] || 1.0).to_f
+
+    @@last_gc_run = Time.now
+
+    def begin_gc_deferment
+      GC.disable if DEFERRED_GC_THRESHOLD > 0
+    end
+
+    def reconsider_gc_deferment
+      if DEFERRED_GC_THRESHOLD > 0 && Time.now - @@last_gc_run >= DEFERRED_GC_THRESHOLD
+        GC.enable
+        GC.start
+        GC.disable
+
+        @@last_gc_run = Time.now
+      end
+    end
+
+    teardown :scrub_instance_variables
+
+    @@reserved_ivars = %w(@loaded_fixtures @test_passed @fixture_cache @method_name @_assertion_wrapped @_result)
+
+    def scrub_instance_variables
+      (instance_variables - @@reserved_ivars).each do |ivar|
+        instance_variable_set(ivar, nil)
+      end
+    end
+  end
 end
